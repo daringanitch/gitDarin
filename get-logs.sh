04629b65046331a61a4c4c4df182df7bac1baaab
daringanitch <dganitch@gmail.com>
2016-03-23 11:53:02 -0700

    modded get-logs script 2

--- get-logs.sh
+++ get-logs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#var is the server id of amazon instance
+#var is the server ip of amazon instance
 var=$( ec2-metadata -o | cut -f 2 -d ':' |tr -d ' ' )
 
 aws s3 sync (your directory here)  s3://(s3 directory here)/$var
