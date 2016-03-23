26fa4bf8a8f35200415cbcddb95b2c9880556fe9
daringanitch <dganitch@gmail.com>
2016-03-23 11:52:11 -0700

    modded get-logs script

--- get-logs.sh
+++ get-logs.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
+#var is the server id of amazon instance
 var=$( ec2-metadata -o | cut -f 2 -d ':' |tr -d ' ' )
 
-aws s3 sync /opt/wildfly/standalone/log/  s3://wholeloan-auction-temp/development/logs/sit/$var
-
-
-*/15 * * * * /root/get-logs.sh >/dev/null 2>&1
+aws s3 sync (your directory here)  s3://(s3 directory here)/$var
