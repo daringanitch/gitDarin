bb14f9ce7444cd4bdd27a3c5d34e4518ade3cafc
daringanitch <dganitch@gmail.com>
2016-09-08 09:18:47 -0700

    InstallPostgres-9-5.sh
    script to install postgres 9.5 on Ubuntu 14.04

---
+++ InstallPostgres-9-5.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
+sleep 10
+apt-get update
+sleep 10
+apt-get install -y postgresql postgresql-contrib
+end
